629ede0fcaf44263d58161b49a9796b93fee614e
yuliia.kyrychok <yuliia.kyrychok@oa.edu.ua>
2022-10-21 02:31:00 +0300

    Users managament added

--- PuzzleShop/PuzzleShop.Repos/Dto/PuzzleDto.cs
+++ PuzzleShop/PuzzleShop.Repos/Dto/PuzzleDto.cs
@@ -1,6 +1,7 @@
 ﻿using PuzzleShop.Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace PuzzleShop.Repos.Dto
     public class PuzzleDto
     {
         public int? Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Articul { get; set; }
--- PuzzleShop/PuzzleShop.UI/Program.cs
+++ PuzzleShop/PuzzleShop.UI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using PuzzleShop.Core;
 using PuzzleShop.Repos;
 
@@ -7,17 +8,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("PuzzleShopConnection");
-builder.Services.AddDbContext<PuzzleShopContext>(options =>
-    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<PuzzleShopContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<PuzzleShopContext>();
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 5;
+}).AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<PuzzleShopContext>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<BrandRepository>();
 builder.Services.AddTransient<CountryRepository>();
 builder.Services.AddTransient<PuzzleRepository>();
+builder.Services.AddTransient<UsersRepository>();
+
 
 var app = builder.Build();
 
